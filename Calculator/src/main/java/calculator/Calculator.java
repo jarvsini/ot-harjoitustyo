56599c42b6bdc89dfaa229c6eefee76844280c47
Sini Järveläinen <sini.jarvelainen@helsinki.fi>
2021-04-12 01:14:07 +0300

    nappeja luotu ja näkymää aseteltu

--- Calculator/src/main/java/calculator/Calculator.java
+++ Calculator/src/main/java/calculator/Calculator.java
@@ -5,54 +5,49 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Calculator extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Button n7 = new Button("7");
-        Button n8 = new Button("8");
-        Button n9 = new Button("9");
-        Button n4 = new Button("4");
-        Button n5 = new Button("5");
-        Button n6 = new Button("6");
-        Button n1 = new Button("1");
-        Button n2 = new Button("2");
-        Button n3 = new Button("3");
-        Label tulos = new Label("0");
+        
+        BorderPane view = new BorderPane();
         
         GridPane buttons = new GridPane();
-
-        for (int x = 1; x <= 3; x++) {
+        
+        int n=1;
+        for (int x = 3; x >= 1; x--) {
             for (int y = 1; y <= 3; y++) {
-                buttons.add(new Button("" + x + ", " ), x, y);
+                buttons.add(new Button(""+n), y, x);
+                n++;
             }
         }
+        
+        buttons.add(new Button("."), 1, 4);
+        buttons.add(new Button("0"), 2, 4);
+        buttons.add(new Button("="), 3, 4);
+        
+        buttons.add(new Button("/"), 4, 1);
+        buttons.add(new Button("x"), 4, 2);
+        buttons.add(new Button("-"), 4, 3);
+        buttons.add(new Button("+"), 4, 4);
+        
+        
+        buttons.setHgap(10);
+        buttons.setVgap(10);
+            
+        view.setCenter(buttons);
+        view.setTop(new TextField());
 
-        
-        FlowPane komponentit = new FlowPane();
-        komponentit.getChildren().add(tulos);
-        komponentit.getChildren().add(n7);
-        komponentit.getChildren().add(n8);
-        komponentit.getChildren().add(n9);
-        komponentit.getChildren().add(n4);
-        komponentit.getChildren().add(n5);
-        komponentit.getChildren().add(n6);
-        komponentit.getChildren().add(n1);
-        komponentit.getChildren().add(n2);
-        komponentit.getChildren().add(n3);
-        
-        
-        komponentit.setPrefSize(400, 600);
-        
-        
-
-
-        Scene scene = new Scene(komponentit);
-        stage.setTitle("Laskin");
+        Scene scene = new Scene(view);
+        stage.setTitle("Calculator");
         stage.setScene(scene);
         stage.show();
     }
